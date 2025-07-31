0652ed132ed63d8ea685f5c02285064019c65c9d
allan raymond <raymonda916@gmail.com>
2025-07-31 22:00:57 +0300

    Update server.js logic/routing

--- server.js
+++ server.js
@@ -1,13 +1,58 @@
 // create a server 
 import http from 'http' // module included with node js!
-const  PORT = 8000
+import fs from 'fs/promises'
+import url from 'url'
+import path from 'path'
+const  PORT = process.env.PORT;
 
-const server = http.createServer((req,res) => {
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+console.log(__filename, __dirname)
+
+const server = http.createServer(async(req,res) => {
+
+ try {
+    if(req.method === 'GET') {
+        let filePath;
+        if (req.url === '/') {
+            filePath = path.join(__dirname, 'public', 'index.html' )
+            // res.writeHead(200, {'Content-Type': 'text/html'})
+            // res.end("<h1>Homepage</h1>")
+    } else if(req.url === '/about') {
+        filePath = path.join(__dirname, 'public', 'about.html' )
+        // res.writeHead(200, {'Content-Type': 'text/html'})
+        //     res.end("<h1>About</h1>")
+    } else {
+        throw new Error('Not Found')
+        // res.writeHead(404, {'Content-Type': 'text/html'})
+        // res.end("<h1>Not Found</h1>")
+    }
+
+    const data = await fs.readFile(filePath)
+    res.setHeader('Content-Type', 'text/html')
+    res.write(data)
+    res.end()
+
+    } else {
+        throw new Error('Method not allowed')
+
+    }
+
+ } catch {
+    res.writeHead(500, {'Content-Type': 'text/plain'})
+        res.end("Server Error")
+    
+
+ }
     // res.setHeader('Content-Type', 'text/html')
     //res.statusCode = 404
     // res.write('Hello World')
-    res.writeHead(500, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify({message: 'Server Error'}))
+
+    // console.log(req.url)
+    // console.log(req.method)
+    // res.writeHead(500, {'Content-Type': 'application/json'})
+    // res.end(JSON.stringify({message: 'Server Error'}))
 
 } )
 
