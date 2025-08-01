ac0402619d0ef4d5cdc80be09c10a4e0a5aac394
allan raymond <raymonda916@gmail.com>
2025-08-01 15:23:38 +0300

    Add osDemo.js: explore Node.js OS module

---
+++ osDemo.js
@@ -0,0 +1,17 @@
+import os from 'os'; // info abt ur system
+
+//userInfo()
+
+console.log(os.userInfo())
+
+//totalMem()
+
+console.log(os.totalmem())
+
+//freemem
+
+console.log(os.freemem())
+
+//cpus
+
+console.log(os.cpus())
