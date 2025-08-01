e6a1a0e361b0f972ef7596dd8fd3a2d03e99ed1c
allan raymond <raymonda916@gmail.com>
2025-08-01 15:23:38 +0300

    Add urlDemo.js: parse and format URLs using Node.js

---
+++ urlDemo.js
@@ -0,0 +1,22 @@
+import url from 'url';
+
+const urlString = 'https://www.google.com/search?q=hello+world';
+
+// URL object
+const urlObj = new URL(urlString)
+console.log(urlObj)
+console.log(urlObj.pathname)
+
+//format()
+console.log(url.format(urlObj))
+
+// file url
+console.log(import.meta.url)
+
+//file url to path -- file path()
+console.log(url.fileURLToPath(import.meta.url))
+
+const params = URLSearchParams(urlObj.search)
+console.log(params)
+console.log(params.get('q')) // or just  console.log(url.format(urlObj.search))
+
